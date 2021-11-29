153bd1aeb82e2df5f7fb0ec6f5209ce31b88033c
Zakaria-Shahen <zikoshahen1@gmial.com>
2021-11-29 03:19:19 +0200

    add and Edit UI

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -1,11 +1,16 @@
 import { Link } from 'react-router-dom'
+import ErrorIcon from '@mui/icons-material/Error';
 
 const NotFound = props => {
 
 
     return (
-        <div>
-            <h1>Not Found Page OR Not Login</h1>
+        <div style={{ margin: "15px auto", width: "80%"}}>
+            <div style={{display: "flex", alignItems: "center"}}>
+                <ErrorIcon sx={{ fontSize: "100px", marginRight: "20px" }} color="warning" />
+                <h1>404: Page Not Found OR Not Login</h1>
+
+            </div>
             <p>
                 if not Login. <Link to="/Login">Click Here</Link>
             </p>
